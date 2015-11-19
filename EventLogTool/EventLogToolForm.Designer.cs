da9929b23ef3925b7f1eb13815ec7148897b3072
Rely Ky <relyky@hotmail.com>
2015-11-19 14:44:37 +0800

    first commit

---
+++ EventLogTool/EventLogToolForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace EventLogTool
+{
+    partial class EventLogToolForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMachineName = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboEntryType = new System.Windows.Forms.ComboBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(243, 39);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check Exists";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(243, 68);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMachineName);
+            this.groupBox1.Controls.Add(this.txtSource);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 89);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Event Log";
+            // 
+            // txtMachineName
+            // 
+            this.txtMachineName.Location = new System.Drawing.Point(88, 53);
+            this.txtMachineName.Name = "txtMachineName";
+            this.txtMachineName.Size = new System.Drawing.Size(100, 22);
+            this.txtMachineName.TabIndex = 3;
+            this.txtMachineName.Text = ".";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(88, 24);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(100, 22);
+            this.txtSource.TabIndex = 2;
+            this.txtSource.Text = "MySource";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Machine Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtOutput.Location = new System.Drawing.Point(12, 179);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(471, 146);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Message";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(408, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Write Entry";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Entry Type";
+            // 
+            // cboEntryType
+            // 
+            this.cboEntryType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEntryType.FormattingEnabled = true;
+            this.cboEntryType.Location = new System.Drawing.Point(78, 141);
+            this.cboEntryType.Name = "cboEntryType";
+            this.cboEntryType.Size = new System.Drawing.Size(324, 20);
+            this.cboEntryType.TabIndex = 7;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(76, 113);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(326, 22);
+            this.txtMessage.TabIndex = 4;
+            this.txtMessage.Text = "This is a message.";
+            // 
+            // EventLogToolForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 337);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.cboEntryType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnCheck);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "EventLogToolForm";
+            this.Text = "EventLogToolForm";
+            this.Load += new System.EventHandler(this.EventLogToolForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtMachineName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboEntryType;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
---
+++ EventLogTool/EventLogToolForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+using NCCUCS.AspectW;
+
+namespace EventLogTool
+{
+    public partial class EventLogToolForm : Form
+    {
+        public EventLogToolForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EventLogToolForm_Load(object sender, EventArgs e)
+        {
+            // init. cboEntryType
+            ArrayList cboEntryTypeItems = new ArrayList();
+            cboEntryTypeItems.Add(new KeyValuePair<EventLogEntryType, string>(EventLogEntryType.Information, EventLogEntryType.Information.ToString()));
+            cboEntryTypeItems.Add(new KeyValuePair<EventLogEntryType, string>(EventLogEntryType.SuccessAudit, EventLogEntryType.SuccessAudit.ToString()));
+            cboEntryTypeItems.Add(new KeyValuePair<EventLogEntryType, string>(EventLogEntryType.Warning, EventLogEntryType.Warning.ToString()));
+            cboEntryTypeItems.Add(new KeyValuePair<EventLogEntryType, string>(EventLogEntryType.Error, EventLogEntryType.Error.ToString()));
+            cboEntryTypeItems.Add(new KeyValuePair<EventLogEntryType, string>(EventLogEntryType.FailureAudit, EventLogEntryType.FailureAudit.ToString()));
+            //
+            cboEntryType.DataSource = cboEntryTypeItems;
+            cboEntryType.ValueMember = "Key";
+            cboEntryType.DisplayMember = "Value";
+            cboEntryType.SelectedIndex = 0;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            AspectW.Define
+                .WaitCursor(this, btnCheck)
+                .Validate(txtSource.Text.Trim() != string.Empty, "Source為必填。")
+                .Validate(txtMachineName.Text.Trim() != string.Empty, "Machine Name為必填。")
+                .Ignore()
+                .TraceException((ex) => txtOutput.AppendText(ex.Message + "\r\n"))
+                .Do(() =>
+                {
+                    string source = txtSource.Text.Trim();
+                    string machineName = txtMachineName.Text.Trim();
+                    if (EventLog.SourceExists(source, machineName))
+                    {
+                        txtOutput.AppendText("已存在。\r\n");
+                    }
+                    else
+                    {
+                        txtOutput.AppendText("不存在。\r\n");
+                    }
+                });
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            AspectW.Define
+                .WaitCursor(this, btnCheck)
+                .Validate(txtSource.Text.Trim() != string.Empty, "Source為必填。")
+                .Validate(txtMachineName.Text.Trim() != string.Empty, "Machine Name為必填。")
+                .Ignore()
+                .TraceException((ex) => txtOutput.AppendText(ex.Message + "\r\n"))
+                .Do(() =>
+                {
+                    string source = txtSource.Text.Trim();
+                    string machineName = txtMachineName.Text.Trim();
+                    EventLog.CreateEventSource(source, source);
+                });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AspectW.Define
+            .WaitCursor(this, btnCheck)
+            .Validate(txtSource.Text.Trim() != string.Empty, "Source為必填。")
+            .Validate(txtMachineName.Text.Trim() != string.Empty, "Machine Name為必填。")
+            .Ignore()
+            .TraceException((ex) => txtOutput.AppendText(ex.Message + "\r\n"))
+            .Do(() =>
+            {
+                string source = txtSource.Text.Trim();
+                string machineName = txtMachineName.Text.Trim();
+                string message = txtMessage.Text;
+                EventLogEntryType eventType = (EventLogEntryType)cboEntryType.SelectedValue;
+
+                using(EventLog loger = new EventLog(source, machineName, source))
+                {
+                    loger.WriteEntry(message, eventType);
+                    txtOutput.AppendText("已寫入一筆Log到系統。\r\n");
+                }
+            });
+
+
+        }
+
+    }
+}
